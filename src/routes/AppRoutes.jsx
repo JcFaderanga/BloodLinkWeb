f577c0d2f104c1f8ce35c6513e754ca7aed65039
Jeysi/ <119491303+JcFaderanga@users.noreply.github.com>
2025-01-20 23:31:46 +0800

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,7 +9,7 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
